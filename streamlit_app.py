abb8d4ec41f24158908db21b1c34cebbbce5967f
adamsnguyen <84123650+adamsnguyen@users.noreply.github.com>
2024-08-08 19:16:04 +0000

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,13 +128,15 @@ if st.session_state.authorized:
 
     # Pagination controls in the sidebar
     with colprev:
-        if st.sidebar.button("Previous Page", disabled=st.session_state.sidebar_page == 0):
-            st.session_state.sidebar_page -= 1
-            st.rerun()
+        with st.container():
+            if st.button("Previous Page", disabled=st.session_state.sidebar_page == 0, use_container_width=True):
+                st.session_state.sidebar_page -= 1
+                st.rerun()
 
     with colnext:
-        if st.sidebar.button("Next Page", disabled=st.session_state.sidebar_page >= total_pages - 1):
-            st.session_state.sidebar_page += 1
-            st.rerun()
+        with st.container():
+            if st.button("Next Page", disabled=st.session_state.sidebar_page >= total_pages - 1, use_container_width=True):
+                st.session_state.sidebar_page += 1
+                st.rerun()
 
     
