8a39f60d0ee4435b7ebcb5c9205f2524e52031aa
adamsnguyen <84123650+adamsnguyen@users.noreply.github.com>
2024-08-08 08:10:58 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if st.session_state.authorized:
     def select_option(index, key):
         st.session_state.answers[index] = key
         rerun = False
-        if st.session_state[f"answer{index}"] not None:
+        if st.session_state[f"answer{index}"] is not None:
             rerun = (key == st.session_state[f"answer{index}"])
         st.session_state[f"answer{index}"] = (key == questions[index]['correct_answer'])
         if rerun:
