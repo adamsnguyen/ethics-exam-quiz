f42b1d474d26c89affe1c5ef95eeaa059d4d8ff4
adamsnguyen <84123650+adamsnguyen@users.noreply.github.com>
2024-08-08 01:47:07 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.session_state.update(st.session_state)
 # Connect to MongoDB
 uri = st.secrets["uri"]
 client = MongoClient(uri, server_api=ServerApi('1'), tls=True)  # 5000
-db = client.db("general")
+db = client.db("questions")
 
 try:
     # Attempt to get the server information to verify the connection
